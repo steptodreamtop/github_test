24ec122f4aeb9216c9529af4d65feecbee4b07b6
steptodreamone <2323348253@qq.com>
2018-01-21 11:13:15 +0800

    1

--- src/yxxy/c_019/MyContainer3.java
+++ src/yxxy/c_019/MyContainer3.java
@@ -1,26 +1,36 @@
 /**   
-* @Title: Mycontainer3.java 
-* @Package yxxy.c_019 
-* @Description:  
-* @author 代富有   
-* @date 2018年1月20日 下午6:13:04 
-* @version V1.0   
-*/
+ * @Title: Mycontainer3.java 
+ * @Package yxxy.c_019 
+ * @Description:  
+ * @author 代富有   
+ * @date 2018年1月20日 下午6:13:04 
+ * @version V1.0   
+ */
 package yxxy.c_019;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-/** 
- * @ClassName: Mycontainer3 
- * @Description:  
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+/**
+ * @ClassName: Mycontainer3
+ * @Description: 曾经的面试题：淘宝 实现一个容器，提供两个方法，add，size
+ *               写两个线程，线程2添加10个元素到容器中，线程2监控元素的个数，当个数达到5个时，线程2给出提示并结束
+ *               给list添加volatile之后，t2能够接到通知，但是，t2线程的死循环很浪费cpu，如果不用死循环，该怎么做？
+ * 
+ *               这里使用wait和notify做到，wait会释放锁，而notify不会释放锁
+ *               需要注意的是，运用这种方法，必须保证t2先执行，也就是首先让t2监听才可以
+ * 
+ *               阅读下面的程序，并分析输出结果 可以读到输出结果并不是size=5时t2退出，而是t1结束时t2才接收到通知而退出
+ *               想想这是为什么？ 因为：wait会释放锁，而notify，sleep不会释放锁
  * @author 代富有
- * @date 2018年1月20日 下午6:13:04 
- *  
+ * @date 2018年1月20日 下午6:13:04
+ * 
  */
 public class MyContainer3 {
-	//添加volatile，使t2能够得到通知
+	// 添加volatile，使t2能够得到通知
 	volatile List lists = new ArrayList();
 
 	public void add(Object o) {
@@ -33,35 +43,53 @@ public class MyContainer3 {
 
 	public static void main(String[] args) {
 		final MyContainer3 c = new MyContainer3();
+		final Object lock = new Object();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (lock) {
+					System.out.println("t2 启动");
+					if (c.size() != 5) {
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+
+					}
+					System.out.println("t2 结束");
+				}
+			}
+		}, "t2").start();
+
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				for (int i = 0; i < 10; i++) {
-					c.add(new Object());
-					System.out.println("add" + i);
-					try {
-						TimeUnit.SECONDS.sleep(1);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+				System.out.println("t1 启动");
+				synchronized (lock) {
+					for (int i = 0; i < 10; i++) {
+						c.add(new Object());
+						System.out.println("add " + i);
+						if (c.size() == 5) {
+							lock.notify();
+						}
+						try {
+							TimeUnit.SECONDS.sleep(1);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}
 		}, "t1").start();
-
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				while (true) {
-					if (c.size() == 5) {
-						break;
-					}
-				}
-				System.out.println("t2 结束");
-			}
-		}, "t2").start();
 
 	}
 }
