b22ce284838de238911f1348128462bfb6b90217
steptodreamone <2323348253@qq.com>
2018-01-25 20:24:29 +0800

    1

--- src/yxxy/c_020/ReentrantLock5.java
+++ src/yxxy/c_020/ReentrantLock5.java
@@ -1,20 +1,55 @@
 /**   
-* @Title: ReentrantLock5.java 
-* @Package yxxy.c_020 
-* @Description:  
-* @author 代富有   
-* @date 2018年1月20日 下午6:14:56 
-* @version V1.0   
-*/
+ * @Title: ReentrantLock5.java 
+ * @Package yxxy.c_020 
+ * @Description:  
+ * @author 代富有   
+ * @date 2018年1月20日 下午6:14:56 
+ * @version V1.0   
+ */
 package yxxy.c_020;
 
-/** 
- * @ClassName: ReentrantLock5 
- * @Description:  
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @ClassName: ReentrantLock5
+ * @Description: reentrantlock用于替代synchronized 由于m1锁定this，只有m1执行完，m2才能执行
+ *               这里是复习synchronized最原始的语义
+ * 
+ *               使用reentrantlock可以完成同样的功能 需要注意的是，必须要手动释放锁
+ *               使用synochronized锁定的话如果遇到异常
+ *               ，jvm会自动释放锁，但是lock必须手动释放锁，因此经常在finally中进行锁的释放
+ * 
+ *               使用reentrantlock可以进行“尝试锁定”trylock，这样无法锁定，或者在指定时间内无法锁定，
+ *               线程可以决定是否继续等待
+ * 
+ *               使用ReentrantLock还可以调用lockInterruptibly方法，可以对线程interrupt方法做出响应，
+ *               在一个线程等待锁的过程中，可以被打断
+ * 
+ *               ReentrantLock还可以指定为公平锁
  * @author 代富有
- * @date 2018年1月20日 下午6:14:56 
- *  
+ * @date 2018年1月20日 下午6:14:56
+ * 
  */
-public class ReentrantLock5 {
+public class ReentrantLock5 extends Thread {
+	private static ReentrantLock lock = new ReentrantLock(true);// 参数为ture表示为公平锁，请对比输出结果
+
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			lock.lock();
+			try {
+				System.out.println(Thread.currentThread().getName() + "获得锁");
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		ReentrantLock5 rl = new ReentrantLock5();
+		Thread th1 = new Thread(rl);
+		Thread th2 = new Thread(rl);
+		th1.start();
+		th2.start();
+	}
 
 }
