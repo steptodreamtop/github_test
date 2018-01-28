98cb1ca29197fc09d2bf0479b54467d8b97cc512
steptodreamone <2323348253@qq.com>
2018-01-28 09:18:15 +0800

    1

--- src/yxxy/c_022/ThreadLocal1.java
+++ src/yxxy/c_022/ThreadLocal1.java
@@ -1,20 +1,55 @@
 /**   
-* @Title: ThreadLocal1.java 
-* @Package yxxy.c_022 
-* @Description:  
-* @author 代富有   
-* @date 2018年1月20日 下午6:15:56 
-* @version V1.0   
-*/
+ * @Title: ThreadLocal1.java 
+ * @Package yxxy.c_022 
+ * @Description:  
+ * @author 代富有   
+ * @date 2018年1月20日 下午6:15:56 
+ * @version V1.0   
+ */
 package yxxy.c_022;
 
-/** 
- * @ClassName: ThreadLocal1 
- * @Description:  
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName: ThreadLocal1
+ * @Description: ThreadLocal线程局部变量
  * @author 代富有
- * @date 2018年1月20日 下午6:15:56 
- *  
+ * @date 2018年1月20日 下午6:15:56
+ * 
  */
 public class ThreadLocal1 {
+	volatile static Person p = new Person();
 
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					TimeUnit.SECONDS.sleep(2);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(p.name);
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				p.name = "lisi";
+			}
+		}).start();
+
+	}
+}
+
+class Person {
+	String name = "zhangsan";
 }
--- src/yxxy/c_022/ThreadLocal2.java
+++ src/yxxy/c_022/ThreadLocal2.java
@@ -1,20 +1,60 @@
 /**   
-* @Title: ThreadLocal2.java 
-* @Package yxxy.c_022 
-* @Description:  
-* @author 代富有   
-* @date 2018年1月20日 下午6:16:09 
-* @version V1.0   
-*/
+ * @Title: ThreadLocal2.java 
+ * @Package yxxy.c_022 
+ * @Description:  
+ * @author 代富有   
+ * @date 2018年1月20日 下午6:16:09 
+ * @version V1.0   
+ */
 package yxxy.c_022;
 
-/** 
- * @ClassName: ThreadLocal2 
- * @Description:  
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName: ThreadLocal2
+ * @Description: ThreadLocal线程局部变量
+ * 
+ *               ThreadLocal是使用空间换时间，synchronized是使用时间换空间
+ *               比如在hibernate中session就存在与ThreadLocal中，避免synchronized的使用
+ *               运行下面的程序，理解ThreadLocal
  * @author 代富有
- * @date 2018年1月20日 下午6:16:09 
- *  
+ * @date 2018年1月20日 下午6:16:09
+ * 
  */
 public class ThreadLocal2 {
+	// volatile static Person p=new Person();
+	static ThreadLocal<Person> tl = new ThreadLocal<>();
 
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					TimeUnit.SECONDS.sleep(2);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(tl.get());
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				tl.set(new Person());
+			}
+		}).start();
+
+	}
+
+	static class Person {
+		String name = "zhangsan";
+	}
 }
