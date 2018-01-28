29baa22057877d8844dcff96efbb52582961a768
steptodreamone <2323348253@qq.com>
2018-01-28 16:18:37 +0800

    1

--- src/yxxy/c_025/T01_ConcurrentMap.java
+++ src/yxxy/c_025/T01_ConcurrentMap.java
@@ -8,6 +8,15 @@
 */
 package yxxy.c_025;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.CountDownLatch;
 /** 
  * @ClassName: T01_ConcurrentMap 
  * @Description:  
@@ -16,5 +25,39 @@ package yxxy.c_025;
  *  
  */
 public class T01_ConcurrentMap {
-
+public static void main(String[] args) {
+//	Map<String, String> map=new ConcurrentHashMap<>();
+//	Map<String, String> map=new ConcurrentSkipListMap<>();
+	final Map<String, String> map=new Hashtable<>();
+//	Map<String, String> map=new HashMap<>();
+	final Random r=new Random();
+	Thread[] ths=new Thread[100];
+	final CountDownLatch latch=new CountDownLatch(ths.length);
+	long start=System.currentTimeMillis();
+	for (int i = 0; i < ths.length; i++) {
+		ths[i]=new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for (int j = 0; j < 10000; j++) {
+					map.put("a"+r.nextInt(10000), "a"+r.nextInt(10000));
+				}
+				latch.countDown();
+			}
+			
+		});
+		//Arrays.asList(ths).forEach(t->t.start());
+		try {
+			latch.await();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		long end=System.currentTimeMillis();
+		System.out.println(end-start);
+		
+	}
+	
+}
 }
